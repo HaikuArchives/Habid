1b86ad6c3027ee2c7444355931a3c4059032de54
tomas.wilhelmsson <tomas.wilhelmsson@47beb7ee-0eb9-11df-ac45-33b0ac6a2133>
2010-03-12 11:38:40 +0000

    Wrote BDataIO_BMemoryIO.cpp and it seems to work, so committing

--- trunk/hcb/include/support/HCB_DataIO.h
+++ trunk/hcb/include/support/HCB_DataIO.h
@@ -93,5 +93,37 @@ public:
 
 /* end file DataIO_BPositionIO.cpp */
 
+class BMemoryIOBridge : public BMemoryIO
+{
+public:
+	BMemoryIOBridge(void *buffer, size_t length);
+	BMemoryIOBridge(const void *buffer, size_t length);
+	virtual ~BMemoryIOBridge();
+};
+
+class BMemoryIOProxy : public BPositionIOProxy, public BMemoryIOBridge
+{
+private:
+	void *fBindInstPointer;
+public:
+	BMemoryIOProxy(void *bindInstPointer, void *data, size_t length);
+	BMemoryIOProxy(void *bindInstPointer, const void *data, size_t length);
+	virtual ~BMemoryIOProxy();
+	
+	virtual ssize_t ReadAt(off_t position, void *buffer, size_t size);
+	virtual ssize_t super_ReadAt(off_t position, void *buffer, size_t size);
+	
+	virtual ssize_t WriteAt(off_t position, const void *buffer, size_t size);
+	virtual ssize_t super_WriteAt(off_t position, const void *buffer, size_t size);
+
+	virtual off_t Seek(off_t position, uint32 seekMode);
+	virtual off_t super_Seek(off_t position, uint32 seekMode);
+		
+	virtual off_t Position() const;
+	virtual off_t super_Position() const;
+
+	virtual status_t SetSize(off_t size);
+	virtual status_t super_SetSize(off_t size);
+};
 
 #endif
