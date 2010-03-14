ba6b7f9664e6b061ebcee591b7e129ccc0d8c73d
tomas.wilhelmsson <tomas.wilhelmsson@gmail.com>
2010-03-14 13:38:37 +0000

    Completed BFlattenable .. i think hope it works

--- hdb/src/Support/Flattenable.d
+++ hdb/src/Support/Flattenable.d
@@ -6,28 +6,28 @@ import Support.SupportDefs;
 import tango.stdc.posix.sys.types;
 
 extern (C) {
-	bool bind_BFlattenable_IsFixedSize(void *) {
-		return true;
+	bool bind_BFlattenable_IsFixedSize(void *instPointer) {
+		return (cast(BFlattenable)instPointer).IsFixedSize();
 	}
 	
-	type_code bind_BFlattenable_TypeCode(void *) {
-		return type_code.DUMMY;
+	type_code bind_BFlattenable_TypeCode(void *instPointer) {
+		return (cast(BFlattenable)instPointer).TypeCode();
 	}
 	
-	ssize_t bind_BFlattenable_FlattenedSize(void *) {
-		return 0;
+	ssize_t bind_BFlattenable_FlattenedSize(void *instPointer) {
+		return (cast(BFlattenable)instPointer).FlattenedSize();
 	}
 	
-	status_t bind_BFlattenable_Flatten(void *, void *, ssize_t) {
-		return 0;
+	status_t bind_BFlattenable_Flatten(void *instPointer, void *buffer, ssize_t size) {
+		return (cast(BFlattenable)instPointer).Flatten(buffer[0..size]);
 	}
 	
-	bool bind_BFlattenable_AllowsTypeCode(void *, type_code) {
-		return 0;
+	bool bind_BFlattenable_AllowsTypeCode(void *instPointer, type_code code) {
+		return (cast(BFlattenable)instPointer).AllowsTypeCode(code);
 	}
 	
-	status_t bind_BFlattenable_Unflatten(void *, type_code, void *, ssize_t) {
-		return 0;
+	status_t bind_BFlattenable_Unflatten(void *instPointer, type_code code, void *buffer, ssize_t size) {
+		return (cast(BFlattenable)instPointer).Unflatten(code, buffer[0..size]);
 	}
 }
 
@@ -45,8 +45,8 @@ public:
 	abstract type_code TypeCode();
 
 	abstract ssize_t FlattenedSize();
-	abstract status_t Flatten(void *buffer, ssize_t size);
-	abstract status_t Unflatten(type_code type, void *buffer, ssize_t size); 
+	abstract status_t Flatten(void [] buffer);
+	abstract status_t Unflatten(type_code type, void [] buffer); 
 	
 	bool AllowsTypeCode(type_code type) {
 		return (TypeCode() == type);
