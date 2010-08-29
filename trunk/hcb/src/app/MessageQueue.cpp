7e8e216419e5c18ad3d8ebe978fa5e1ac492e39d
tomas.wilhelmsson <tomas.wilhelmsson@47beb7ee-0eb9-11df-ac45-33b0ac6a2133>
2010-08-29 18:52:24 +0000

    Added MessageRunner, Messenger in both C and D

--- trunk/hcb/src/app/MessageQueue.cpp
+++ trunk/hcb/src/app/MessageQueue.cpp
@@ -12,79 +12,80 @@ extern "C" {
 
 /* end import functions */
 
-/* begin class BMessageQueueBridge */
-
-BMessageQueueBridge::BMessageQueueBridge()
+/* begin class BMessageQueue */
+/*
+BMessageQueue::BMessageQueue()
 : BMessageQueue()
 { }
 
-BMessageQueueBridge::~BMessageQueueBridge()
+BMessageQueue::~BMessageQueue()
 { }
 
-bool BMessageQueueBridge::IsLocked() const
+bool BMessageQueue::IsLocked() const
 {
 	return BMessageQueue::IsLocked();
 }
-
-/* end class BMessageQueueBridge */
+*/
+/* end class BMessageQueue */
 
 /* begin export functions */
 
 extern "C" {
 	be_BMessageQueue * be_BMessageQueue_ctor() {
-		return (be_BMessageQueue *)new BMessageQueueBridge();
+		return (be_BMessageQueue *)new BMessageQueue();
 	}
 	
 	void be_BMessageQueue_dtor(be_BMessageQueue *instPointer) {
-		delete (BMessageQueueBridge *)instPointer;
+		delete (BMessageQueue *)instPointer;
 	}
 
 	void be_BMessageQueue_AddMessage(be_BMessageQueue *instPointer, be_BMessage* message) {
-		((BMessageQueueBridge *)instPointer)->AddMessage((BMessage *)message);
+		((BMessageQueue *)instPointer)->AddMessage((BMessage *)message);
 	}
 	
 	void be_BMessageQueue_RemoveMessage(be_BMessageQueue *instPointer, be_BMessage* message) {
-		((BMessageQueueBridge *)instPointer)->RemoveMessage((BMessage *)message);
+		((BMessageQueue *)instPointer)->RemoveMessage((BMessage *)message);
 	}
 
 
 	int32 be_BMessageQueue_CountMessages(be_BMessageQueue *instPointer) {
-		return ((BMessageQueueBridge *)instPointer)->CountMessages();
+		return ((BMessageQueue *)instPointer)->CountMessages();
 	}
 
 	bool be_BMessageQueue_IsEmpty(be_BMessageQueue *instPointer) {
-		return ((BMessageQueueBridge *)instPointer)->IsEmpty();
+		return ((BMessageQueue *)instPointer)->IsEmpty();
 	}
 
 
 	be_BMessage* be_BMessageQueue_FindMessage_1(be_BMessageQueue *instPointer, int32 index) {
-		return (be_BMessage *)((BMessageQueueBridge *)instPointer)->FindMessage(index);
+		return (be_BMessage *)((BMessageQueue *)instPointer)->FindMessage(index);
 	}
 
 	be_BMessage* be_BMessageQueue_FindMessage_2(be_BMessageQueue *instPointer, uint32 what, int32 index) {
-		return (be_BMessage *)((BMessageQueueBridge *)instPointer)->FindMessage(what, index);
+		return (be_BMessage *)((BMessageQueue *)instPointer)->FindMessage(what, index);
 	}
 
 
 	bool be_BMessageQueue_Lock(be_BMessageQueue *instPointer) {
-		return ((BMessageQueueBridge *)instPointer)->Lock();
+		return ((BMessageQueue *)instPointer)->Lock();
 	}
 
 	void be_BMessageQueue_Unlock(be_BMessageQueue *instPointer) {
-		((BMessageQueueBridge *)instPointer)->Unlock();
+		((BMessageQueue *)instPointer)->Unlock();
 	}
 
+	/* Is this realy right? */
 	bool be_BMessageQueue_IsLocked(be_BMessageQueue *instPointer) {
-		return ((BMessageQueueBridge *)instPointer)->IsLocked();
+		return ((const BMessageQueue *)instPointer)->IsLocked();
 	}
 
 
 	be_BMessage *be_BMessageQueue_NextMessage(be_BMessageQueue *instPointer) {
-		return (be_BMessage *)((BMessageQueueBridge *)instPointer)->NextMessage();
+		return (be_BMessage *)((BMessageQueue *)instPointer)->NextMessage();
 	}
 
 	bool be_BMessageQueue_IsNextMessage(be_BMessageQueue *instPointer, const be_BMessage* message) {
-		return ((BMessageQueueBridge *)instPointer)->IsNextMessage((const BMessage *)message);
+		return ((BMessageQueue *)instPointer)->IsNextMessage((const BMessage *)message);
 	}
 }
 
