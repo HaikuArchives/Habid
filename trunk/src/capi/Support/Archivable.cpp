fe0fda6f6ea20bc7891f8ecb0092bcc7a3ef6105
tomas.wilhelmsson <tomas.wilhelmsson@47beb7ee-0eb9-11df-ac45-33b0ac6a2133>
2010-09-16 16:44:30 +0000

    Added archivable, tho without Unarchiver and Archiver

---
+++ trunk/src/capi/Support/Archivable.cpp
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2010 Tomas Wilhelmsson <tomas.wilhelmsson@gmail.com>
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+
+#include "Support/Archivable.h"
+
+BArchivableBridge::BArchivableBridge()
+: BArchivable()
+{
+}
+
+
+BArchivableBridge::BArchivableBridge(BMessage* from)
+: BArchivable(from)
+{
+}
+
+
+BArchivableBridge::~BArchivableBridge() { }
+
+
+
+
+
+
+
+
+
+
+
+BArchivableProxy::BArchivableProxy(void *bindInstPtr)
+: fBindInstPtr(bindInstPtr), BArchivableBridge() { }
+
+BArchivableProxy::BArchivableProxy(void *bindInstPtr, BMessage* from)
+: fBindInstPtr(bindInstPtr), BArchivableBridge(from) { }
+
+BArchivableProxy::~BArchivableProxy() { }
+
+status_t BArchivableProxy::Archive(BMessage* into, bool deep) const
+{
+	return bind_BArchivable_Archive(fBindInstPtr, into, deep);
+}
+
+status_t BArchivableProxy::Archive_super(BMessage* into, bool deep) const
+{
+	return BArchivable::Archive(into, deep);
+}
+
+status_t BArchivableProxy::Perform(perform_code d, void* arg) 
+{
+	return bind_BArchivable_Perform(fBindInstPtr, d, arg);
+}
+
+status_t BArchivableProxy::Perform_super(perform_code d, void* arg) 
+{
+	return BArchivable::Perform(d, arg);
+}
+
+status_t BArchivableProxy::AllUnarchived(const BMessage* archive) 
+{
+	return bind_BArchivable_AllUnarchived(fBindInstPtr, archive);
+}
+
+status_t BArchivableProxy::AllUnarchived_super(const BMessage* archive) 
+{
+	return BArchivable::AllUnarchived(archive);
+}
+
+status_t BArchivableProxy::AllArchived(BMessage* archive) const
+{
+	return bind_BArchivable_AllArchived(fBindInstPtr, archive);
+}
+
+status_t BArchivableProxy::AllArchived_super(BMessage* archive) const
+{
+	return BArchivable::AllArchived(archive);
+}
+
+
+extern "C" {
+	BArchivableProxy * be_BArchivable_ctor(void *bindInstPtr)
+	{
+		return new BArchivableProxy(bindInstPtr);
+	}
+
+	BArchivableProxy * be_BArchivable_ctor_1(void *bindInstPtr, BMessage* from)
+	{
+		return new BArchivableProxy(bindInstPtr, from);
+	}
+
+	void be_BArchivable_dtor(BArchivable* self)
+	{
+		delete self;
+	}
+
+	status_t be_BArchivable_Archive(BArchivableProxy *self, BMessage* into, bool deep)
+	{
+		return self->Archive_super(into, deep);
+	}
+
+	status_t be_BArchivable_Perform(BArchivableProxy *self, perform_code d, void* arg)
+	{
+		return self->Perform_super(d, arg);
+	}
+
+	status_t be_BArchivable_AllUnarchived(BArchivableProxy *self, const BMessage* archive)
+	{
+		return self->AllUnarchived_super(archive);
+	}
+
+	status_t be_BArchivable_AllArchived(BArchivableProxy *self, BMessage* archive)
+	{
+		return self->AllArchived_super(archive);
+	}
+
+}
+
---
+++ trunk/src/capi/include/Support/Archivable.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2010 Tomas Wilhelmsson <tomas.wilhelmsson@gmail.com>
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+
+#ifndef HABID_BArchivable_
+#define HABID_BArchivable_
+
+#include <Archivable.h>
+
+class BArchivableBridge
+: public BArchivable
+{
+public:
+	BArchivableBridge();
+	BArchivableBridge(BMessage* from);
+	~BArchivableBridge();
+
+};
+
+
+class BArchivableProxy
+: public BArchivableBridge
+{
+	void *fBindInstPtr;
+public:
+	BArchivableProxy(void *bindInstPtr);
+	BArchivableProxy(void *bindInstPtr, BMessage* from);
+	~BArchivableProxy();
+
+	virtual status_t Archive(BMessage* into, bool deep) const;
+	virtual status_t Archive_super(BMessage* into, bool deep) const;
+	virtual status_t Perform(perform_code d, void* arg) ;
+	virtual status_t Perform_super(perform_code d, void* arg) ;
+	virtual status_t AllUnarchived(const BMessage* archive) ;
+	virtual status_t AllUnarchived_super(const BMessage* archive) ;
+	virtual status_t AllArchived(BMessage* archive) const;
+	virtual status_t AllArchived_super(BMessage* archive) const;
+};
+
+
+/* BArchivable */
+extern "C" {
+	status_t bind_BArchivable_Archive(void *bindInstPtr, BMessage* into, bool deep);
+	status_t bind_BArchivable_Perform(void *bindInstPtr, perform_code d, void* arg);
+	status_t bind_BArchivable_AllUnarchived(void *bindInstPtr, const BMessage* archive);
+	status_t bind_BArchivable_AllArchived(void *bindInstPtr, BMessage* archive);
+}
+#endif // HABID_BArchivable_
+
