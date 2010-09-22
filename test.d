7bc2c186f224a3383807043aee1ced64bfb636b2
tomas.wilhelmsson <tomas.wilhelmsson@gmail.com>
2010-09-22 15:27:37 +0000

    Added BlockCache, dont realy know how to handle this class as of yet, so have to be revisited later, seems to work with simple tests atleast

--- test.d
+++ test.d
@@ -7,34 +7,21 @@ import Support.DataIO;
 
 import Support.SupportDefs;
 // import tango.stdc.posix.sys.types;
-
-class MyMemoryIO : BMemoryIO
-{
-public:
-	this(void [] buffer) { super(buffer); }
-	off_t Seek(off_t position, uint32 seekMode) {
-		Stdout.formatln("Seek Performed: {} SeekMode: {}", position, seekMode);
-		return super.Seek(position, seekMode);
-	}
-}
+import Support.BlockCache;
+import tango.stdc.stdlib;
 
 int main() {
-	void [] buffer = new void[256];
+	BBlockCache cache = new BBlockCache(10, 256, B_OBJECT_CACHE);
 
-	MyMemoryIO io = new MyMemoryIO(buffer);
+	void *data2 = cast(void *)malloc(256);
 
-	char [] str = fromStringz("Hello, MemoryIO");
 
-	io.Write(str, str.length + 1);
+	void [] data = cache.Get(128);
 
-	char [] buffer2 = new char[256];
+	Stdout.formatln("data length: {}", data.length);
 
-	io.Seek(0, 0);
-
-	auto x = cast(void [])buffer2;
-	io.Read(x, str.length + 1);
-
-	Stdout.formatln("Buffer2: {}", cast(char [])buffer2);
+	cache.Save(data.ptr, 128);
+	cache.Save(data2, 256);
 
 	return 0;
 }
